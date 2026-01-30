358c55ea0b5c93977379964e79759d2fb3952835
Dove Stack <flawal664@gmail.com>
2026-01-30 13:42:38 +0100

    added TrustedBy Component

--- src/App.jsx
+++ src/App.jsx
@@ -45,6 +45,7 @@ import axios from "axios";
 import { Icon } from "@iconify/react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import TrustedBy from "./components/TrustedBy/TrustedBy";
 
 function App() {
   const [file, setFile] = useState(null);
@@ -109,53 +110,11 @@ function App() {
 
   return (
     <div className="min-h-screen bg-page-gradient text-slate-900">
-      {/* Navigation */}
       <Navbar />
-      {/* Hero Section */}
 
       <Hero />
 
-      <div className="relative max-w-5xl mx-auto mt-16 py-7">
-        <div className="bg-white rounded-xl shadow-xl border border-gray-100 overflow-hidden">
-          <div className="bg-white border-b border-gray-50 px-4 py-3 flex gap-2">
-            <div className="w-3 h-3 rounded-full bg-red-400"></div>
-            <div className="w-3 h-3 rounded-full bg-yellow-400"></div>
-            <div className="w-3 h-3 rounded-full bg-green-400"></div>
-          </div>
-
-          {/* Chat Body */}
-          <div className="bg-slate-50/50 px-6 py-6 sm:px-8 sm:py-6 flex flex-col gap-4">
-            {/* User Message */}
-            <div className="flex items-center gap-4">
-              <div className="p-2.5 rounded-full bg-blue-100 flex items-center justify-center">
-                <Icon
-                  icon="icon-park-outline:user"
-                  className="w-5 h-5 text-brand-navy"
-                />
-              </div>
-
-              <div className="bg-slate-200 text-dull-text px-4 py-3 rounded-lg w-full text-sm sm:text-base">
-                Can you summarize the key findings from this research paper?
-              </div>
-            </div>
-
-            {/* AI Message */}
-            <div className="flex items-start gap-4">
-              <div className="bg-brand-navy text-white shadow-md px-4 py-3 rounded-lg w-full text-sm sm:text-base leading-relaxed">
-                Based on the document, here are the 5 key findings: 1. AI
-                adoption increased by 47% in 2024...
-              </div>
-
-              <div className="p-2.5 rounded-full bg-indigo-100 flex items-center justify-center  order-last">
-                <Icon
-                  icon="mingcute:head-ai-line"
-                  className="w-5 h-5 text-brand-navy"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <TrustedBy />
     </div>
   );
 }

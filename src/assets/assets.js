fa4e755e1ba63e529652b765bfdc3ecb07221964
Dove Stack <flawal664@gmail.com>
2026-01-30 15:00:57 +0100

    Add university and company image imports to assets.js
    - Imported and exported logos for UniLag, UniLorin, CovenantUni, MoniePoint, DHL, Chowdeck, S&P, and TechRadar
    - Centralizes asset management for easy use in components

---
+++ src/assets/assets.js
@@ -0,0 +1,21 @@
+import UniLag from "./Unilag.png";
+import UniLorin from "./Unilorin.png";
+import CoveUni from "./CovenantUni.png";
+import MoniePoint from "./MoniePoint.png";
+import DHL from "./DHL.png";
+import Chowdeck from "./Chowdeck.png";
+import SP from "./S_P.png";
+import TechRadar from "./TechRadar.png";
+
+
+
+export {
+    UniLag,
+    UniLorin,
+    CoveUni,
+    MoniePoint,
+    DHL,
+    Chowdeck,
+    SP,
+    TechRadar
+}

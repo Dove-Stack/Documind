7948bf4ec4a1d09affe7081fbe27e7348072f290
Dove Stack <flawal664@gmail.com>
2026-01-30 23:52:28 +0100

    Refactor Features section to use dynamic data mapping
    - Refactor Features section to replace duplicated static content with dynamic data mapping
    
    - Centralized feature content, icons, and colors in a data array
    - Improved maintainability and scalability of the Features section
    
    This also Improve Features section by refactoring static feature blocks into reusable data-driven structure

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { Icon } from "@iconify/react";
+import React from "react";
+import { featuresData } from "../../assets/assets";
 
 const Feature = () => {
   return (
-    <div>Feature</div>
-  )
-}
+    <div className="flex flex-col gap-1 mt-10 items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-2">
+        <h3 className="text-xl font-medium tracking-tight mb-1 text-center  text-brand-gradient">
+          FEATURES
+        </h3>
+        <h2 className="text-4xl font-semibold tracking-tight text-center text-brand-navy">
+          Everything You Need for Smarter Research
+        </h2>
+        <p className="text-sm w-4/6 text-center text-slate-text">
+          DocuMind combines powerful AI technology with an intuitive interface
+          to transform how you work with documents.
+        </p>
+      </div>
 
-export default Feature
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 p-8">
+        {featuresData.map((feature, index) => (
+          <div
+            key={index}
+            className="group p-8 rounded-2xl bg-white/50 shadow-sm transition-alll duration-300"
+          >
+            <div
+              className={`inline-flex p-3 rounded-xl mb-6 ${feature.bgColor}`}
+            >
+              <Icon
+                icon={feature.icon}
+                // className="w-6 h-6 text-feature-upload"
+                className={`w-6 h-6 ${feature.iconColor}`}
+              ></Icon>
+            </div>
+
+            <p className=" font-semibold text-brand-navy mb-3">
+              {feature.title}
+            </p>
+
+            <span className="text-sm text-slate-text leading-relaxed">
+              {feature.description}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Feature;
